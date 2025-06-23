cc98d0a9fd45e5e3eb57b0edd3de0cf25497193e
Alex <manyakinsasha@gmail.com>
2025-06-23 22:31:16 +0300

    add layout, split layout into components

--- react-pizza-app/src/App.tsx
+++ react-pizza-app/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./scss/app.scss";
+import { Categories, Header, Sort } from "./components/shared";
+import { PizzaBlock } from "./components/shared/pizza-block.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body>
+        <div className="wrapper">
+          <Header />
+          <div className="content">
+            <div className="container">
+              <div className="content__top">
+                <Categories />
+                <Sort />
+              </div>
+              <h2 className="content__title">Все пиццы</h2>
+              <div className="content__items">
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+                <PizzaBlock/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ react-pizza-app/src/components/shared/index.ts
@@ -0,0 +1,3 @@
+export {Header} from './header';
+export {Categories} from './categories';
+export {Sort} from './sort';
