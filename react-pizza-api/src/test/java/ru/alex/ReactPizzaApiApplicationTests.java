15ec89dc077b066f18b15f3408284e3a1cae7310
Alex <manyakinsasha@gmail.com>
2025-06-24 21:28:59 +0300

    add unversioned file

---
+++ react-pizza-api/src/test/java/ru/alex/ReactPizzaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactPizzaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
